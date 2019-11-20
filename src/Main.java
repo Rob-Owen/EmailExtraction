1cfdf87028a21a9f546ea2b2709360dc05c76b60
Robert Owen <robert.owen@softwire.com>
2019-11-20 13:40:13 +0000

    Part 3 & some stretch goals

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,44 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    private final static Pattern EMAIL = Pattern.compile("(?<=^|\\s)(?:[\\w.'_%+-]+)@((?:[a-zA-Z0-9-]+\\.)+[a-zA-Z0-9-]+)(?=$|\\s)");
+    private static HashMap<String, Integer> domainCount = new HashMap<>();
+
+    /**
+     * Command line arguments:
+     * 1 - Occurrence threshold to print
+     */
+    public static void main(String[] args) throws IOException {
         var file = new File("sample.txt");
-        var br = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8));
-        var email = Pattern.compile("(\\s|^)([\\w.'_%+-]+)@(([\\w-]+\\.)+[\\w-]+)(\\s|$)");
+        var reader = new BufferedReader(new FileReader(file, StandardCharsets.UTF_8));
 
-        String line;
-        int startIndex;
-        var lineNum = 1;
-
-        while ((line = br.readLine()) != null) {
-            var m = email.matcher(line);
-            startIndex = 0;
-            while (m.find(startIndex)) {
-                System.out.print(lineNum + " ");
-                System.out.println(m.group(0));
-                startIndex = m.end(5);
+        reader.lines().forEach(line -> {
+            var m = EMAIL.matcher(line);
+            while (m.find()) {
+                registerEmailDomain(m.group(1));
             }
-            lineNum += 1;
+        });
+
+        domainCount.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .limit(10)
+                .filter(e -> e.getValue() > Integer.parseInt(args[0]))
+                .forEach(e -> System.out.println(e.getKey() + ": " + e.getValue()));
+    }
+
+    private static void registerEmailDomain(String domain) {
+        if (domainCount.containsKey(domain)) {
+            domainCount.put(domain, domainCount.get(domain) + 1);
+        } else {
+            domainCount.put(domain, 1);
         }
     }
 }
